b14e4d4675e8f52345b2eabc1253d172b1d72dff
rezkitr <rtriandy@gmail.com>
2019-07-08 15:02:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 </head>
 <body>
 
+<h1>ALIF JANCOK</h1>
+
 <h2>Generate PDF file from MySQL Using PHP</h2>
 <form class="form-inline" method="post" action="generate_pdf.php">
 <button type="submit" id="pdf" name="generate_pdf" class="btn btn-primary"><i class="fa fa-pdf"" aria-hidden="true"></i>
