6bc283820f5ce470731b2d9978b3442ba4c2fd5e
Poorlady <saniekasmara@gmail.com>
2019-07-10 09:57:14 +0700

    zip

--- generate_pdf.php
+++ generate_pdf.php
@@ -378,9 +378,23 @@ while ($row = mysqli_fetch_assoc($karyawan)) {
 
     //$pdf->Output('D', 'Coba.pdf');
 }
-$count = count($);
-echo $count;
+$zip = new ZipArchive;
+if ($zip->open('test_new.zip', ZipArchive::CREATE) === TRUE)
+{
+    // Add files to the zip file
+    $zip->addFile('test.txt');
+    $zip->addFile('test.pdf');
+ 
+    // Add random.txt file to zip and rename it to newfile.txt
+    $zip->addFile('random.txt', 'newfile.txt');
+ 
+    // Add a file new.txt file to zip using the text specified
+    $zip->addFromString('new.txt', 'text to be added to the new.txt file');
+ 
+    // All files are added, so close the zip file.
+    $zip->close();
+}
 
-$pdf->output();
+$pdf->output("F");
 
 ?>
